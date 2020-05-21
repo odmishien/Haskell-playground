df00747e22e6fc115c9af951ca5ef903da8f325c
odmishien <tmtm.yagi.007@gmail.com>
2020-05-21 22:03:15 +0900

    runnin on message

--- web-sample/app/Main.hs
+++ web-sample/app/Main.hs
@@ -9,9 +9,9 @@ import           Network.Wai.Handler.Warp       ( run )
 import           Data.List                      ( lookup )
 import           Data.Maybe
 
-main :: IO ()
-
-main = run 8080 helloApp
+main = do
+    putStrLn "running on http://localhost:8080"
+    run 8080 helloApp
 
 helloApp :: Application
 helloApp req res = res $ responseLBS status200 [] "hello"
